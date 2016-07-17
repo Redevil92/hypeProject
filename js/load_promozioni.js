a9d3ef27e34d3ab235f86b020835d8c9c84b3a88
Redevil <stefano.badalucco@mail.polimi.it>
2016-07-17 20:51:10 +0200

    something mew/ mewtwo

--- js/load_promozioni.js
+++ js/load_promozioni.js
@@ -4,6 +4,7 @@ function ready(){
 
     getPromozioneFisso();
     getPromMobile();
+    getPromMobileFisso();
 
 
 }
@@ -99,23 +100,47 @@ function getPromMobile() {
 
 
             console.log(response);
+            var i = 0;
 
-            for (var i = 0; i < smartLife.length - 4; i++) {
+            for (; i < prom_mobile.length - 4; i++) {
+                if(i == 0)
+                    output += "<h2 style='color: lightblue; margin: 5%'>VOCE E INTERNET</h2>>"
                 output += "<div  style='margin-top: 20px;  margin: 1%;'>" +
                     "<div class='row'>" +
                     "<div class='col-sm-1'></div>" +
                     "<div class='col-sm-5'>" +
-                    "<h3 style='color: lightblue; align-content: center'>" + prom_mobile[i].TitoloArg + "</h3>" +
+                    "<h3 style='color: lightblue; align-content: center'>" + prom_mobile[i].NomePromozione + "</h3>" +
                     "<h4>" + prom_mobile[i].Descrizione + "</h4>" +
                     "<h4 style='color: red'>" + prom_mobile[i].Prezzo + "</h4>";
                 output += "</div><div class = 'col-sm-5'>" +
                     "<img src=" + prom_mobile[i].Immagine + "></div><div class='col-sm-1'></div></div>" +
-                    "<button type='button' class='btn btn-primary' style='margin-left: 45%; margin-bottom: 10px' >SCOPRI</button>" +
+                    "<button type='button' class='btn btn-primary' style='margin-left: 45%; margin-bottom: 10px' >SCOPRI L'OFFERTA</button>" +
                     "</div>";
 
             }
 
             $("#prom_mobile").html(output);
+
+            var outputEstero = "";
+
+            for(var k = 0; i < prom_mobile.length; i++){
+                if(k++ == 0)
+                    outputEstero +="<h3 style='margin-left: 5%; margin-top: 5%; color: lightblue'>ESTERO</h3>";
+
+
+                outputEstero += "<div style='margin: 5%'><div class='row'><div class ='col-sm-6'><div class = 'row'><div class='col-sm-1'></div>" +
+                    "<div class='col-sm-6'><img src=" + prom_mobile[i].Immagine + "></div><div class='col-sm-4'>" +
+                    "<h3 style='color: lightblue'>" + prom_mobile[i].NomePromozione + "</h3><h4>" + prom_mobile[i].Descrizione + "</h4>" +
+                    "<h4 style='color: red'>" + prom_mobile[i].Prezzo + "</h4><button type='button' class='btn btn-primary'>SCOPRI</button></div>" +
+                    "<div class='col-sm-1'></div> </div></div>"; //end of the first part
+                i++;
+                outputEstero += "<div class ='col-sm-6'><div class = 'row'><div class='col-sm-1'></div>" +
+                    "<div class='col-sm-6'><img src=" + prom_mobile[i].Immagine + "></div><div class='col-sm-4'>" +
+                    "<h3 style='color: lightblue'>" + prom_mobile[i].NomePromozione + "</h3><h4>" + prom_mobile[i].Descrizione + "</h4>" +
+                    "<h4 style='color: red'>" + prom_mobile[i].Prezzo + "</h4><button type='button' class='btn btn-primary'>SCOPRI</button></div></div></div></div>"
+            }
+
+            $("#prom_mobileExt").html(outputEstero);
 
 
         },
@@ -126,3 +151,58 @@ function getPromMobile() {
 
     });
 }
+
+
+function getPromMobileFisso() {
+
+    $.ajax({
+        method: "POST",
+        //dataType: "json", //type of data
+        crossDomain: true, //localhost purposes
+        url: "php/load_promozioni.php", //Relative or absolute path to file.php file
+        success: function (response) {
+            var promozioni = JSON.parse(response);
+            var output = "";
+
+            var prom_fissoPiuMobile = new Array();
+            var j = 0;
+
+            for(var i = 0; i < promozioni.length; i++){
+                if(promozioni[i].Pagina == ("FissoPiuMobile"))
+                    prom_fissoPiuMobile[j++] = promozioni[i];
+            }
+
+
+            console.log(response);
+            var i = 0;
+
+            for (; i < prom_fissoPiuMobile.length; i++) {
+                if(i == 0)
+                    output += "<h2 style='color: lightblue; margin: 5%'>VOCE E INTERNET</h2>>"
+                output += "<div  style='margin-top: 20px;  margin: 1%;'>" +
+                    "<div class='row'>" +
+                    "<div class='col-sm-1'></div>" +
+                    "<div class='col-sm-5'>" +
+                    "<h3 style='color: lightblue; align-content: center'>" + prom_fissoPiuMobile[i].NomePromozione + "</h3>" +
+                    "<h4>" + prom_fissoPiuMobile[i].Descrizione + "</h4>" +
+                    "<h4 style='color: red'>" + prom_fissoPiuMobile[i].Prezzo + "</h4>";
+                output += "</div><div class = 'col-sm-5'>" +
+                    "<img src=" + prom_fissoPiuMobile[i].Immagine + "></div><div class='col-sm-1'></div></div>" +
+                    "<button type='button' class='btn btn-primary' style='margin-left: 40%; margin-bottom: 10px' >SCOPRI L'OFFERTA</button><hr id = 'smartLine'>" +
+                    "</div>";
+
+            }
+
+            $("#prom_fisso_mobile").html(output);
+
+
+
+        },
+
+        error: function (request, error) {
+            console.log("Error");
+        }
+
+    });
+}
+
