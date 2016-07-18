a8257adbc213516dd799571be66c9bc6972e12ab
Dave <davidealessandro.cottica@mail.polimi.it>
2016-07-18 16:01:02 +0200

    tanta roba

--- js/LoadNetworking.js
+++ js/LoadNetworking.js
@@ -11,7 +11,7 @@ function getNetworking() {
             for(var i = 0; i < 3; i++) {
                 sent += '<br><div class = "row text-center">';
                 for(var j = i*3; j < 3*(i+1); j++) {
-                    sent += '<div class="col-sm-4"><h2 class="title_brand">'+ devices[j].Marca + '</h2><p class="title_paragraph">'+ devices[j].Nome +'</p><div class="col-sm-6"><img src="images/' + devices[j].img_path + '" class="imglist img-responsive"></div><div class="col-sm-6"><p>' + devices[j].DescrizioneVeloce +'</p><button class="btn btn-primary">Dettagli</button></div></div>';
+                    sent += '<div class="col-sm-4"><h2 class="title_brand">'+ devices[j].Marca + '</h2><p class="title_paragraph">'+ devices[j].Nome +'</p><div class="col-sm-6"><img src="images/' + devices[j].img_path + '" class="imglist img-responsive"></div><div class="col-sm-6"><p>' + devices[j].DescrizioneVeloce +'</p><a href = "Tim_Device_Detail.html?id='+devices[j].ID+'#modem_networking"><button class="btn btn-primary myButton">Dettagli</button></a></div></div>';
                 }
             sent += '</div><br>';
             }
---
+++ js/LoadSingleDevice.js
@@ -0,0 +1,84 @@
+$(document).ready(URLParser());
+
+function URLParser() {
+    
+    var ref = window.location.href;
+    
+    var page = window.location.pathname;
+    
+    var data = ref.split('?')[1]; //target id is here
+    
+    var value = data.split('=')[1]; //this is the correct value
+    
+    console.log(getQueryParams(ref));
+    
+    console.log(data);
+                                // debug purpose
+    console.log(value);
+    
+    getSingleDevice(value);
+}
+
+function getSingleDevice(value) {
+    
+    var id = value.split('#')[0];       //id in the DB
+    var table = value.split('#')[1];    //table passed to .php file
+    
+    $.ajax({
+        method: "POST",
+        crossDomain: true, //localhost purposes
+        data:{id : id, table : table},
+        url: "php/GetSingleDevice.php", //Relative or absolute path to file.php file
+        success: function(response) {
+            
+            var device = JSON.parse(response);
+            
+            var carousel = '';
+            
+            var rightDesc = '';
+            
+            if(device[0].Prezzo == 0) { //devices not implemented in the DB
+                carousel = '<div class="container"><div class="jumbotron text-center"><h1>'+device[0].Marca+' '+device[0].Nome +'</h1></div><div class="row"><div class="col-sm-6"><img src="images/'+device[0].img_path+'" alt="path"></div>';
+                
+                rightDesc = '<div class="col-sm-6"><div class="well text-center"><h1>Dati per questo dispositivo non disponibili. Consultare il link selezionato sulla pagina principale</h1></div></div></div>';
+            }
+            else { //devices with full description in DB
+                // bootstrap carousel initialization
+                carousel = '<div class="container"><div class="jumbotron text-center"><h1>'+device[0].Marca+' '+device[0].Nome +'</h1></div><div class="row"><div class="col-sm-6"><div id="myCarousel" class="carousel slide" data-ride="carousel" style="margin-top:80px"><!-- Indicators --><ol class="carousel-indicators"><li data-target="#myCarousel" data-slide-to="0" class="active"></li><li data-target="#myCarousel" data-slide-to="1"></li><li data-target="#myCarousel" data-slide-to="2"></li><li data-target="#myCarousel" data-slide-to="3"></li></ol><div class="carousel-inner" role="listbox"><div class="item active"><img src="images/'+device[0].small_img1+'" class="center-block big-img" alt="front"></div><div class="item"> <img src="images/'+device[0].small_img2+'" class="center-block big-img"></div><div class="item"><img src="images/'+device[0].small_img3+'" class="center-block big-img"></div><div class="item"><img src="images/'+device[0].small_img4+'" class="center-block big-img"></div><a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"><span class="icon-prev" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"><span class="icon-next" aria-hidden="true"></span><span class="sr-only">Next</span></a></div></div></div>';
+            
+                //right column
+                rightDesc = '<div class="col-sm-6"><h1>Descrizione</h1><p>'+device[0].InformazioniVeloci+'</p><div class="well text-center"><h3><b>Acquistalo a prezzo pieno</b></h3><h1 class="price">'+device[0].Prezzo+'€</h1><button type="button" class="btn btn-primary">Aggiungi al carrello</button></div><div class="well text-center"><h4>Scegli una delle offerte TIM e ricevi questo device a soli</h4><h1> class="price">'+device[0].PrezzoAbbonamento+'€/mese</h1><button type="button" class="btn btn-primary">Abbina all offerta</button></div></div>';
+            }
+
+             
+            var technical ='';
+            
+            technical = '<div class="row"><ul class="nav nav-tabs bottomtabs"><li class="active"><a data-toggle="tab" href="#home">Scheda Tecnica</a></li><li><a data-toggle="tab" href="#menu1">Consegna</a></li><li><a data-toggle="tab" href="#menu2">Recesso e garanzia</a></li></ul><div class="tab-content"><div id="home" class="tab-pane fade in active"><pre style="margin-left:10px">'+device[0].Descrizione+'</pre></div><div id="menu1" class="tab-pane fade"><h3>Boh</h3><p>Some content in menu 1.</p></div><div id="menu2" class="tab-pane fade"><h3>Altro Boh</h3><p>Some content in menu 2.</p></div></div></div>';
+            
+            var topString = carousel + rightDesc +'</div><hr>';
+            
+            $("#product_overview").html(topString);
+            
+            $("#technical_tab").html(technical);
+        
+        },
+        error: function(request,error)
+        {
+            console.log("Error");
+        }
+    });
+}
+
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
